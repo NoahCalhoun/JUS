b9eda76b41d364c239eac0141517451e286bec69
skal5758 <skal1579@naver.com>
2018-03-17 02:50:04 +0900

    Thread
    쓰레드 관련 define 추가
    쓰레드 관련 실험 코드 추가

--- GameSvr/define.h
+++ GameSvr/define.h
@@ -2,6 +2,13 @@
 
 #ifdef UNICODE
 using STRING = wstring;
+#define TO_STRING(n) to_wstring(n)
 #else
 using STRING = string;
+#define TO_STRING(n) to_string(n)
 #endif // !UNICODE
+
+using LOCK = recursive_mutex;
+using LOCK_GUARD = lock_guard<recursive_mutex>;
+
+#define synchronized(n) if (lock_guard<recursive_mutex>(n), FALSE) {} else
